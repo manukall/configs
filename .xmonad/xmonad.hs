e872a7867b1ed0a0933b2426af253f502c42610d
Manuel Kallenbach <manuel.kallenbach@gmail.com>
2011-04-30 19:07:22 +0200

    more layouts, im layout for skype

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,13 +1,19 @@
+import Data.Ratio ((%))
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Layout.ResizableTile
+import XMonad.Layout.IM
+import XMonad.Layout.Grid
+import XMonad.Layout.Tabbed
+import XMonad.Layout.PerWorkspace
 import XMonad.Actions.CycleWS
 import XMonad.Actions.GridSelect
 import XMonad.Actions.WindowBringer
 import qualified XMonad.StackSet as W
+import XMonad.Layout.NoBorders
 import System.IO
 
 myManageHook = composeAll
@@ -19,13 +25,31 @@ myManageHook = composeAll
   ]
 
 myLayout = ResizableTall 1 (3/100) (1/2) []
+imLayout = avoidStruts $ IM (1%7) (Title "manukall - Skype™ (Beta)")
+tabbedLayout = tabbed shrinkText tabbedConf
+ 
+tabbedConf = defaultTheme {
+    fontName = "xft:Terminus"
+}
+
+genericLayouts = avoidStruts $
+                 smartBorders $
+                 {-toggleLayouts (noBorders Full) $-}
+                 Mirror tiled ||| tiled ||| tabbedLayout ||| (noBorders Full)
+  where
+      tiled = Tall 1 (3 / 100) (3 / 4)
+
+myLayouts = onWorkspace "4:skype" imLayout $
+            genericLayouts
+
+
 myWorkspaces    = ["1:main","2:work","3:web","4:skype","5:mail", "6:media", "7", "8", "9:chat"]
 
 main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ defaultConfig
     { manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
-    , layoutHook = avoidStruts $ layoutHook defaultConfig ||| myLayout
+    , layoutHook = myLayouts
     , logHook = dynamicLogWithPP $ xmobarPP
       { ppOutput = hPutStrLn xmproc
       , ppTitle = xmobarColor "green" "" . shorten 50
